d46baeebe95bdf2f0602cee81a4fa97cbbfedf8c
matin2510 <mustafizurmatin@ac.c4q.nyc>
2017-12-20 21:28:59 -0500

    complete part02

---
+++ app/src/main/java/nyc/c4q/mustafizurmatin/midunitassessment/Books.java
@@ -0,0 +1,40 @@
+package nyc.c4q.mustafizurmatin.midunitassessment;
+
+/**
+ * Created by c4q on 12/20/17.
+ */
+
+public class Books {
+    private String title;
+    private String author;
+    private int year;
+
+    public Books() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+
+}
--- app/src/main/java/nyc/c4q/mustafizurmatin/midunitassessment/MainActivity.java
+++ app/src/main/java/nyc/c4q/mustafizurmatin/midunitassessment/MainActivity.java
@@ -4,8 +4,18 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "onCretae";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +29,77 @@ public class MainActivity extends AppCompatActivity {
         fragmentTransaction.replace(R.id.main_container, mainFragment);
         fragmentTransaction.commit();
 
+        JSONObject newJsonWeAreMaking = new JSONObject();
+        try {
+            newJsonWeAreMaking.put("books", new JSONArray()
+                    .put(new JSONObject()
+                            .put("title", "Northanger Abbey")
+                            .put("author", "Austen, Jane")
+                            .put("year", 1814))
+                    .put(new JSONObject()
+                            .put("title", "War and Peace")
+                            .put("author", "Tolstoy, Leo")
+                            .put("year", 1865))
+                    .put(new JSONObject()
+                            .put("title", "Anna Karenina")
+                            .put("author", "Tolstoy, Leo")
+                            .put("year", 1875))
+                    .put(new JSONObject()
+                            .put("title", "Mrs. Dalloway")
+                            .put("author", "Woolf, Virginia")
+                            .put("year", 1925))
+                    .put(new JSONObject()
+                            .put("title", "The Hours")
+                            .put("author", "Cunnningham, Michael")
+                            .put("year", 1999))
+                    .put(new JSONObject()
+                            .put("title", "Huckleberry Finn")
+                            .put("author", "Twain, Mark")
+                            .put("year", 1865))
+                    .put(new JSONObject()
+                            .put("title", "Bleak House")
+                            .put("author", "Dickens, Charles")
+                            .put("year", 1870))
+
+                    .put(new JSONObject()
+                            .put("title", "Tom Sawyer")
+                            .put("author", "Twain, Mark")
+                            .put("year", 1862))
+
+
+            );
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        String jsonString = newJsonWeAreMaking.toString();
+
+        Log.d(TAG, "onCreate: " + jsonString);
+        try {
+            JSONObject oldJsonWeAreParsing = new JSONObject(jsonString);
+            JSONArray booksArrayList = oldJsonWeAreParsing.getJSONArray("family_members");
+            List<Books> booksList = new ArrayList<>();
+
+            for (int i = 0; i < booksArrayList.length(); i++) {
+                Books books = new Books();
+
+
+                books.setTitle((String) booksArrayList.getJSONObject(i).get("title"));
+                books.setAuthor((String) booksArrayList.getJSONObject(i).get("author"));
+                books.setYear((Integer) booksArrayList.getJSONObject(i).get("year"));
+                booksList.add(books);
+            }
+
+            for (Books books : booksList) {
+                Log.d(TAG, "onCreate: " +
+                        "\nTitle - " + books.getTitle() +
+                        "\nAuthor - " + books.getAuthor() +
+                        "\nYear - " + books.getYear());
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
+
+
 }
